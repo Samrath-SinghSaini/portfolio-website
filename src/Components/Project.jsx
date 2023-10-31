97d02863317a83529e47df22f4cc0c7db75d4ce3
Samrath Singh Saini <62909539+SamrathSinghSaini@users.noreply.github.com>
2023-10-30 23:48:13 -0700

    added live link for keeper app

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -8,7 +8,7 @@ function Project() {
     <div className="project-main">
     <p className="project-note">Please note that the projects are hosted on a free service with a shared plan, website will take longer to load.</p>
     <ProjectTile title="Electronic Ecommerce Store Application" description={Description[4]} src='./images/ecom6.png' stack={['NodeJS', 'Express','React', 'MongoDB']} link='https://electronic-ecommerce-store.vercel.app/'/>
-      <ProjectTile title="Keeper App" description={Description[0]} src='./images/keeper.png' stack={['NodeJS', 'Express','React', 'MongoDB']}/>
+      <ProjectTile title="Keeper App" description={Description[0]} src='./images/keeper.png' stack={['NodeJS', 'Express','React', 'MongoDB']} link='https://keeper-app-two-eta.vercel.app/'/>
       <ProjectTile title='Travel and Tourism Management System' description={Description[1]} src='./images/tourismJava.png' stack={['Java', 'Swing']}/>
       <ProjectTile title='Blog Website' description={Description[2]} src='./images/journal.png' stack={['NodeJS', 'Express','EJS']}/>
       <ProjectTile title='Tetris' description={Description[3]} src='./images/tet2.png' stack={['Swift']}/>
